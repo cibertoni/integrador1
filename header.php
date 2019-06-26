2c94205d500d4ddc7b5546b0b63332c9d75336ad
Ricardo Ortega <rortegas@gmail.com>
2019-06-26 13:03:07 -0300

    upload inicial do arquivo global header.php

---
+++ header.php
@@ -0,0 +1,15 @@
+<header>
+  <nav class="global">
+    <img src="" alt="Blend Café logo">
+    <ul>
+      <li><a href="#">home</a></li>
+      <li><a href="#">loja</a></li>
+      <li><a href="#">sobre</a></li>
+      <li><a href="#">contato</a></li>
+    </ul>
+    <a href="#">
+      <img src="" alt="carrinho">
+    </a>
+    <a class="button" href="#">login</a>
+  </nav>
+</header>
