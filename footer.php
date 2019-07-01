35dd881188906fb728ffb994c5c79ddba735c515
Ricardo Ortega <rortegas@gmail.com>
2019-07-01 13:06:52 -0300

    HTML básico do header e footer.

---
+++ footer.php
@@ -0,0 +1,19 @@
+<footer>
+  <div class="info">
+    <p>Café Blend</p>
+    <p>Endereço: Av. Dr. Cardoso de Melo, 90</p>
+    <p>Telefone: 9-9999-9999</p>
+  </div>
+  <nav>
+    <ul>
+      <li><a href="#">home</a></li>
+      <li><a href="#">loja</a></li>
+      <li><a href="#">sobre</a></li>
+      <li><a href="#">contato</a></li>
+    </ul>
+    <form class="newsletter" action="index.html" method="post">
+      <input type="email" name="email" placeholder="nome@email.com">
+      <button type="submit" name="subscribe">Assinar</button>
+    </form>
+  </nav>
+</footer>
--- header.php
+++ header.php
@@ -1,15 +1,13 @@
 <header>
   <nav class="global">
-    <img src="" alt="Blend Café logo">
+    <h1>Blend Café</h1>
     <ul>
       <li><a href="#">home</a></li>
       <li><a href="#">loja</a></li>
       <li><a href="#">sobre</a></li>
       <li><a href="#">contato</a></li>
     </ul>
-    <a href="#">
-      <img src="" alt="carrinho">
-    </a>
+    <a href="#">compras</a>
     <a class="button" href="#">login</a>
   </nav>
 </header>
